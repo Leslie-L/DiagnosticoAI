232bfbd1cf85151ff220d2c670476071989d5752
Leslie-L <leslie.centeno@galileo.edu>
2022-09-19 10:26:24 -0600

    cargar imagenes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const ojo = document.getElementById('card_ojo');
 const modal_ocular =document.getElementById('modal-ocular');
-const close =document.getElementById('close');
+const closeM =document.getElementById('close');
 const depresion = document.getElementById('card_depresion');
 const modal_depresion =document.getElementById('modal-depresion');
 const close_depresion =document.getElementById('close_depresion');
@@ -9,10 +9,13 @@ const close_depresion =document.getElementById('close_depresion');
 /*OJO */
 ojo.addEventListener('click', ()=>{
     modal_ocular.classList.remove("hidden");
+
 });
 
-close.addEventListener('click', ()=>{
+closeM.addEventListener('click', ()=>{
     modal_ocular.classList.add("hidden");
+    var imagen = document.getElementById("ojo_nuevo");
+    imagen.src= "~/../images/png.png";
 });
 /*
 Depresion
@@ -24,3 +27,19 @@ depresion.addEventListener('click', ()=>{
 close_depresion.addEventListener('click', ()=>{
     modal_depresion.classList.add("hidden");
 });
+
+
+/*
+CARGAR FOTO
+*/
+function cambiarFoto(){
+    
+    var input=document.getElementById("archivo");
+    var reader = new FileReader(); //Leemos el contenido
+    var imagen = document.getElementById("ojo_nuevo");
+    
+    reader.onload = function(e) { 
+        imagen.src= e.target.result;
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
